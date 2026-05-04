c0769b7f44821b1482983aef5deba6457521a131
Isaac Zheng <isaaczheng0414@gmail.com>
2026-05-04 04:26:01 -0400

    ???

--- evaluation/tasks/longbench.py
+++ evaluation/tasks/longbench.py
@@ -126,6 +126,7 @@ class LongBenchEvaluator(BaseEvaluator):
                         do_sample=False,
                         pad_token_id=tokenizer.eos_token_id,
                         past_key_values=custom_cache,
+                        output_attentions=True,
                         use_cache=True
                     )
                 self._cleanup_cache_and_hooks(custom_cache)
